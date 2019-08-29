336b4b4a0eccd9f70317675f31241a779f775c9c
lorvog <Lorenz.Vossgaetter@adesso.de>
2019-08-29 13:36:44 +0200

    fixed typo in import

--- org/wickedsource/dependencytree/example/somepackage/CircularDependencyTest.java
+++ org/wickedsource/dependencytree/example/somepackage/CircularDependencyTest.java
@@ -1,6 +1,6 @@
 package org.wickedsource.dependencytree.example.somepackage;
 
-import org.wickedsource.dependencytree.example.example.CoreTest;
+import org.wickedsource.dependencytree.example.CoreTest;
 
 public class CircularDependencyTest{
 
