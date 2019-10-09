9253a4bd18b57691fd33c612116d24d7d4cb257c
Johannes Teklote <johannes.teklote@web.de>
2019-10-09 19:07:09 +0200

    fix lost dependency

--- src/org/wickedsource/dependencytree/somepackage/DuplicateDependenciesTest.java
+++ src/org/wickedsource/dependencytree/somepackage/DuplicateDependenciesTest.java
@@ -1,11 +1,15 @@
 package org.wickedsource.dependencytree.somepackage;
 //should not add the same dependency twice
 //same for wildcard imports in order (explicit -> wildcard)
+import org.wickedsource.dependencytree.CoreTest;
 import org.wickedsource.dependencytree.wildcardpackage.*;
 
 public class DuplicateDependenciesTest {
 
+    private CoreTest coreTest;
+
     public DuplicateDependenciesTest() {
+
     }
 }
 
